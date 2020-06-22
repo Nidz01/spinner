f0034344dcbcb9993e8a73afec119c6087ab6e83
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-22 16:48:39 -0400

    Update spinner2.js

--- spinner2.js
+++ spinner2.js
@@ -1,3 +1,7 @@
+/*
+Refactor the logic to remove the repeated setTimeout statements.
+We should instead be left with much less code (about 8 to 10 lines of code).
+*/
 process.stdout.write('hello from spinner2.js... \rHey!!\n');
 let spinner = ["\r|   ", "\r/   ", "\r-   ", "\r\\   "];
 let increment = 100;
